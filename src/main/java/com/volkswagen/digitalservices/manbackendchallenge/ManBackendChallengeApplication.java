2e122ebaa8b68a8f85e6c13f4c4b4e9f9b6a72f4
André Pires <andre.pires@premium-minds.com>
2020-09-26 17:50:50 +0100

    initial commit

---
+++ src/main/java/com/volkswagen/digitalservices/manbackendchallenge/ManBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.volkswagen.digitalservices.manbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volkswagen/digitalservices/manbackendchallenge/ManBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkswagen.digitalservices.manbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
