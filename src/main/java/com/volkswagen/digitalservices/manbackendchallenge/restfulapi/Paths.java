5844868e741df29057621c458a4cef76630dc063
André Pires <andre.pires@premium-minds.com>
2020-09-27 18:24:05 +0100

    add status endpoint

---
+++ src/main/java/com/volkswagen/digitalservices/manbackendchallenge/restfulapi/Paths.java
@@ -0,0 +1,6 @@
+package com.volkswagen.digitalservices.manbackendchallenge.restfulapi;
+
+public final class Paths {
+    public static final String ROOT = "/api";
+    public static final String STATUS = "/status";
+}
